07815239683725722f85babddb5d69b68a5e9c8f
Brian Denicola <bjd145@yahoo.com>
2019-12-01 23:18:18 -0600

    Password History API

---
+++ Source/functionapp/Functions/GetPasswordHistoryById.cs
@@ -0,0 +1,49 @@
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using PasswordService.Common;
+using PasswordService.Models;
+
+namespace PasswordService
+{
+    public static partial class PasswordService
+    {
+        [FunctionName("GetPasswordHistoryById")]
+        public static IActionResult GetPasswordHistoryById(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "passwords/{id}/history")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "%COSMOS_DATABASENAME%",
+                collectionName: "%COSMOS_COLLECTIONNAME%",
+                PartitionKey = "%COSMOS_PARTITIONKEY%",
+                ConnectionStringSetting = "cosmosdb",
+                Id = "{id}")] AccountPassword accountPassword,
+            ILogger log)            
+        {
+            log.LogInformation($"GetPasswordHistoryById request for {accountPassword.id}");
+
+            var history = from oldPassword in accountPassword.OldPasswords
+                select new { 
+                    SiteName = accountPassword.SiteName,
+                    Password = oldPassword.DecryptPassword(e),
+                    TimeStamp = oldPassword.CreatedDate
+                };
+            
+            return (ActionResult)new OkObjectResult(history);
+        }
+    }
+}
