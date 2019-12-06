ea1da11e1b31394649a7596408343515aba031e7
Brian Denicola <bjd145@yahoo.com>
2019-12-06 08:03:02 -0600

    Code improvements

--- BackupFunction/VaultBackup/__init__.py
+++ BackupFunction/VaultBackup/__init__.py
@@ -15,24 +15,16 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 import azure.functions as func
 
-def main(mytimer: func.TimerRequest,
-         outputblob: func.Out[func.InputStream]):
+key=base64.urlsafe_b64decode(os.environ["AesKey"])
+nonce=base64.urlsafe_b64decode(os.environ["AesIV"])
+passwordvaultid=os.environ["VaultSpnId"]
+clientid=os.environ["Clientid"]
+secret=os.environ["SpnSecret"]
+login_url=os.environ["LoginUrl"]
+app_url=os.environ["AppUrl"]
+code=os.environ["FunctionCode"]
 
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
-
-    key=base64.urlsafe_b64decode(os.environ["AesKey"])
-    nonce=base64.urlsafe_b64decode(os.environ["AesIV"])
-    passwordvaultid=os.environ["VaultSpnId"]
-    clientid=os.environ["Clientid"]
-    secret=os.environ["SpnSecret"]
-    login_url=os.environ["LoginUrl"]
-    app_url=os.environ["AppUrl"]
-    code=os.environ["FunctionCode"]
-
-    logging.info('Authenticating against Azure AD')
+def getLoginToken():
     body = { 
         "resource": passwordvaultid, 
         "scope": "openid",
@@ -43,29 +35,46 @@ def main(mytimer: func.TimerRequest,
     r = requests.post(login_url, data=body)
 
     token = json.loads(r.text)
-    bearer_token = "{} {}".format(token['token_type'], token['access_token'])
+    return "{} {}".format(token['token_type'], token['access_token'])
 
-    logging.info('Getting all Passwords')
-    headers = {'Authorization': bearer_token}
-    r = requests.get("{}?code={}".format(app_url,code), headers=headers)
-    passwords = json.loads(r.text)
+def getPasswords(token):
+    header = {'Authorization': token}
+    r = requests.get("{}?code={}".format(app_url,code), headers=header)
+    return json.loads(r.text)
 
-    logging.info('Getting each Password History')
-    backups = []
-    for password in passwords:
-        password_history_url = "{}/{}/history?code={}".format(app_url,password['id'], code)
-        r = requests.get(password_history_url, headers=headers)
-        history = json.loads(r.text)
-        backups.append(history)
+def getPassword(password, token):
+    header = {'Authorization': token}
+    password_history_url = "{}/{}/history?code={}".format(app_url,password['id'], code)
+    r = requests.get(password_history_url, headers=header)
+    return json.loads(r.text)
 
-    logging.info('Encrypting output')
+def encrytPasswordFile(backups):
     aesgcm = AESGCM(key)
     cipher_text_bytes = aesgcm.encrypt(
         nonce=nonce,
         data=json.dumps(backups).encode('utf-8'),
         associated_data=None
     )
+    return cipher_text_bytes
 
-    logging.info('Write encrypted output')
+def main(mytimer: func.TimerRequest,
+         outputblob: func.Out[func.InputStream]):
+
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    logging.info('Authenticating against Azure AD')
+    bearer_token = getLoginToken()
+
+    logging.info('Getting all Passwords')
+    passwords = getPasswords(bearer_token)
+
+    logging.info('Getting each Password History')
+    backups = [getPassword(p, bearer_token) for p in passwords]
+
+    logging.info('Encrypting output')
     with open("encrypted_file.enc", 'wb') as encrypted_file:
-        outputblob.set(cipher_text_bytes)
+        outputblob.set(encrytPasswordFile(backups))
+   
