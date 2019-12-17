c54f1787c586a5f8be24141c26487a501dadd42a
Brian Denicola <bjd145@yahoo.com>
2019-12-17 10:47:48 -0600

    Improvements to Creation Script

--- Infrastructure/create_azure_resources.sh
+++ Infrastructure/create_azure_resources.sh
@@ -22,10 +22,13 @@ funcStorageName=${functionAppName}sa001
 keyVaultName=${functionAppName}keyvault001
 
 # Create an Azure Function with storage accouunt in the resource group.
-az storage account create --name $funcStorageName --location $location --resource-group $RG --sku Standard_LRS
-az functionapp create --name $functionAppName --storage-account $funcStorageName --consumption-plan-location $location --resource-group $RG
-az functionapp identity assign --name $functionAppName --resource-group $RG
-functionAppId="$(az functionapp identity show --name $functionAppName --resource-group $RG --query 'principalId' --output tsv)"
+if ! `az functionapp show --name $functionAppName --resource-group $RG -o none`
+then
+    az storage account create --name $funcStorageName --location $location --resource-group $RG --sku Standard_LRS
+    az functionapp create --name $functionAppName --storage-account $funcStorageName --consumption-plan-location $location --resource-group $RG
+    az functionapp identity assign --name $functionAppName --resource-group $RG
+fi
+functionAppId=`az functionapp identity show --name $functionAppName --resource-group $RG --query 'principalId' --output tsv`
 
 #Cosmos DB
 az cosmosdb create -g $RG -n $cosmosdb --kind GlobalDocumentDB 
@@ -37,20 +40,20 @@ az storage account create --kind StorageV2 --name $storageName --location $locat
 az storage blob service-properties update --account-name $storageName --static-website --404-document "404.html" --index-document "index.html"
 
 # Setup CORS from Storage Account
-webUrl=$(az storage account show -n $storageName -g $RG --query "primaryEndpoints.web" --output tsv)
+webUrl=`az storage account show -n $storageName -g $RG --query "primaryEndpoints.web" --output tsv | sed -e 's/\/$//g'`
 az functionapp cors add -g $RG -n $functionAppName --allowed-origins $webUrl
 
 # Create Key Vault 
 az keyvault create --name $keyVaultName --resource-group $RG --location $location 
 az keyvault set-policy --name $keyVaultName --object-id $functionAppId --secret-permissions get
 
-primaryMasterKey="$(az cosmosdb list-keys -g $RG -n $cosmosdb --query 'primaryMasterKey' --output tsv )"
+primaryMasterKey=`az cosmosdb list-keys -g $RG -n $cosmosdb --query 'primaryMasterKey' --output tsv`
 primaryConnectionString="AccountEndpoint=https://${cosmosdb}.documents.azure.com:443/;AccountKey=${primaryMasterKey};"
-aesKeySecretId="$(az keyvault secret set --vault-name $keyVaultName --name AesKey --value $aesKey --query 'id' --output tsv)"
-primaryConnectionStringSecretId="$(az keyvault secret set --vault-name $keyVaultName --name cosmosdb --value $primaryConnectionString --query 'id' --output tsv)"
+aesKeySecretId=`az keyvault secret set --vault-name $keyVaultName --name AesKey --value $aesKey --query 'id' --output tsv`
+primaryConnectionStringSecretId=`az keyvault secret set --vault-name $keyVaultName --name cosmosdb --value $primaryConnectionString --query 'id' --output tsv`
 
-az functionapp config appsettings set -g $RG -n $functionAppName --settings cosmosdb="@Microsoft.KeyVault(SecretUri=$primaryConnectionStringSecretId)"
-az functionapp config appsettings set -g $RG -n $functionAppName --settings AesKey="@Microsoft.KeyVault(SecretUri=$aesKeySecretId)"
+az functionapp config appsettings set -g $RG -n $functionAppName --settings cosmosdb="@Microsoft.KeyVault(SecretUri=${primaryConnectionStringSecretId})"
+az functionapp config appsettings set -g $RG -n $functionAppName --settings AesKey="@Microsoft.KeyVault(SecretUri=${aesKeySecretId})"
 az functionapp config appsettings set -g $RG -n $functionAppName --settings AesIV=$aesIV
 az functionapp config appsettings set -g $RG -n $functionAppName --settings COSMOS_DATABASENAME="AccountPasswords"
 az functionapp config appsettings set -g $RG -n $functionAppName --settings COSMOS_COLLECTIONNAME="Passwords"
@@ -58,22 +61,22 @@ az functionapp config appsettings set -g $RG -n $functionAppName --settings COSM
 az functionapp config appsettings set -g $RG -n $functionAppName --settings COSMOS_PARTITIONKEY="Passwords"
 
 #Host Key for Functions
-userName=$(az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].userName' --output tsv)
-userPassword=$(az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].userPWD' --output tsv)
-kuduUrl=$(az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].publishUrl' --output tsv)
-adminUrl="https://$kuduUrl/api/functions/admin/token"
-keyUrl="https://$functionAppName.azurewebsites.net/admin/host/keys/main"
+userName=`az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].userName' --output tsv`
+userPassword=`az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].userPWD' --output tsv`
+kuduUrl=`az functionapp deployment list-publishing-profiles -n $functionAppName -g $RG --query '[0].publishUrl' --output tsv`
+adminUrl="https://${kuduUrl}/api/functions/admin/token"
+keyUrl="https://${functionAppName}.azurewebsites.net/admin/host/keys/main"
 
-JWT=$(curl -s -X GET -u $userName:$userPassword $adminUrl | tr -d '"')
-functionHostKey=$(curl -s -X POST -H "Authorization: Bearer $JWT" -H "Content-Type: application/json" -d "Content-Length: 0" $keyUrl | jq -r '.value')
+JWT=`curl -s -X GET -u $userName:$userPassword $adminUrl | tr -d '"'`
+functionHostKey=`curl -s -X POST -H "Authorization: Bearer $JWT" -H "Content-Type: application/json" -d "Content-Length: 0" $keyUrl | jq -r '.value'`
 az keyvault secret set --vault-name $keyVaultName --name functionSecret --value $functionHostKey
 
 # Update Functions with Azure Search Configuration
-searchAdminKey=`az search admin-key show --resource-group ${searchServiceRG} --service-name ${searchServiceName} -o tsv --query "primaryKey"`
-searchAdminKeyId="$(az keyvault secret set --vault-name $keyVaultName --name searchAdminKey --value $searchAdminKey --query 'id' --output tsv)"
-az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_SERVICENAME=${searchServiceName}
-az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_ADMINKEY="@Microsoft.KeyVault(SecretUri=$searchAdminKeyId)"
-az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_INDEXNAME=${searchIndexName}
+searchAdminKey=`az search admin-key show --resource-group $searchServiceRG --service-name $searchServiceName -o tsv --query "primaryKey"`
+searchAdminKeyId=`az keyvault secret set --vault-name $keyVaultName --name searchAdminKey --value $searchAdminKey --query 'id' --output tsv`
+az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_SERVICENAME=$searchServiceName
+az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_ADMINKEY="@Microsoft.KeyVault(SecretUri=${searchAdminKeyId})"
+az functionapp config appsettings set -g $RG -n $functionAppName --settings SEARCH_INDEXNAME=$searchIndexName
 
 # Update Function App for Azure AD Authentication 
-az webapp auth update -g $RG -n $functionAppName --enabled true --action LoginWithAzureActiveDirectory --aad-client-id ${clientID}
+az webapp auth update -g $RG -n $functionAppName --enabled true --action LoginWithAzureActiveDirectory --aad-client-id $clientID
