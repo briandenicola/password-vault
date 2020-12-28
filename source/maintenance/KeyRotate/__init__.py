6d8ff7515df8e4825126e8a7b5a990997fb14bf5
Brian Denicola <bjd145@yahoo.com>
2020-12-28 16:23:43 -0600

    Fixes for race condition on key rotation

--- source/maintenance/KeyRotate/__init__.py
+++ source/maintenance/KeyRotate/__init__.py
@@ -29,23 +29,22 @@ def main(mytimer: func.TimerRequest) -> None:
     cosmos_connection_string_secret = "cosmosdb"
     function_app_setting_value = "cosmosdb"
 
-    key_to_rotate="secondary"
+    key_to_get="primary"; key_to_rotate="secondary"
     if datetime.datetime.now().weekday() % 2 == 0:
-        key_to_rotate="primary"
+        key_to_get="secondary"; key_to_rotate="primary"
 
     cosmosdb_client = CosmosDBManagementClient(
         credential=credential,
         subscription_id=SUBSCRIPTION_ID
     )
 
-    logging.info('[%s] - Update Cosmos DB %s key', utc_timestamp, key_to_rotate)
-    cosmosdb_client.database_accounts.begin_regenerate_key(
-        RESOURCE_GROUP_NAME, 
-        COSMOS_DB_ACCOUNT, 
-        DatabaseAccountRegenerateKeyParameters(key_kind=key_to_rotate)
-    )
+    logging.info('[%s] - Get Cosmos DB %s key', utc_timestamp, key_to_get)
     keys = cosmosdb_client.database_accounts.list_keys(RESOURCE_GROUP_NAME, COSMOS_DB_ACCOUNT)
-    new_connection_string = "AccountEndpoint=https://{0}.documents.azure.com:443/;AccountKey={1};".format(COSMOS_DB_ACCOUNT, keys.primary_master_key)
+
+    if key_to_get == "primary":
+        new_connection_string = "AccountEndpoint=https://{0}.documents.azure.com:443/;AccountKey={1};".format(COSMOS_DB_ACCOUNT, keys.primary_master_key)
+    else:
+        new_connection_string = "AccountEndpoint=https://{0}.documents.azure.com:443/;AccountKey={1};".format(COSMOS_DB_ACCOUNT, keys.secondary_master_key)
 
     logging.info('[%s] - Update Key Vault', utc_timestamp)
     keyvault_client = SecretClient(
@@ -65,3 +64,10 @@ def main(mytimer: func.TimerRequest) -> None:
     app_settings = web_client.web_apps.list_application_settings(RESOURCE_GROUP_NAME, FUNCTION_ACCOUNT)
     app_settings.properties.update({ function_app_setting_value: new_secret_kv_reference })
     web_client.web_apps.update_application_settings(RESOURCE_GROUP_NAME, FUNCTION_ACCOUNT, app_settings=app_settings)
+
+    logging.info('[%s] - Update Cosmos DB %s key', utc_timestamp, key_to_rotate)
+    cosmosdb_client.database_accounts.begin_regenerate_key(
+        RESOURCE_GROUP_NAME, 
+        COSMOS_DB_ACCOUNT, 
+        DatabaseAccountRegenerateKeyParameters(key_kind=key_to_rotate)
+    )
