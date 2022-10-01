ae6080c8a291d0ed6501e1a762cc3448c03dc161
Brian Denicola <brian@briandenicola.com>
2022-09-30 21:13:33 -0500

    New script to upgrade to v4 runtime

--- Scripts/upgrade-v4.sh
+++ Scripts/upgrade-v4.sh
@@ -10,7 +10,6 @@ while (( "$#" )); do
       funcName=$2
       shift 2
       ;;
-      ;;
     -h|--help)
       echo "Usage: ./upgrade-v4.sh -g <resource-group> -n <function-name>
         --resource-group|-g: Resource group name
