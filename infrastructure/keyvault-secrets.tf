a4e4762a9d563746717437173aa50744a34169bf
Brian <brian@briandenicola.com>
2025-01-03 18:42:20 +0000

    Bug fixes

--- infrastructure/keyvault-secrets.tf
+++ infrastructure/keyvault-secrets.tf
@@ -1,22 +1,34 @@
 resource "azurerm_key_vault_secret" "aes_encryption_key" {
+  depends_on = [ 
+    azurerm_role_assignment.deployer_kv_access
+  ]
   name         = "encryption-key"
   value        = var.password_encryption_key
   key_vault_id = azurerm_key_vault.this.id
 }
 
 resource "azurerm_key_vault_secret" "aes_encryption_iv" {
+  depends_on = [ 
+    azurerm_role_assignment.deployer_kv_access
+  ]  
   name         = "initialization-vector"
   value        = var.password_encryption_initialization_vector
   key_vault_id = azurerm_key_vault.this.id
 }
 
 resource "azurerm_key_vault_secret" "host_key" {
+  depends_on = [ 
+    azurerm_role_assignment.deployer_kv_access
+  ]  
   name         = "host-key"
   value        = data.azurerm_function_app_host_keys.host_key.primary_key
   key_vault_id = azurerm_key_vault.this.id
 }
 
 resource "azurerm_key_vault_secret" "cosmosdb_connection_string" {
+  depends_on = [ 
+    azurerm_role_assignment.deployer_kv_access
+  ]  
   name         = "cosmosdb-connection-string"
   value        = azurerm_cosmosdb_account.this.primary_sql_connection_string 
   key_vault_id = azurerm_key_vault.this.id
