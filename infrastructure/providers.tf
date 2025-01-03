e822959d411a2b6bbd9eb174e4cfad26a6baafcb
Brian <brian@briandenicola.com>
2025-01-03 20:56:43 +0000

    backend config

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -6,12 +6,13 @@ terraform {
       version = "~> 4"
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "Core-ResourceGroup"
-  #   storage_account_name = "denicolafamily"
-  #   container_name       = "state"
-  #   key                  = "vault.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "Core-ResourceGroup"
+    storage_account_name = "denicolafamily"
+    subscription_id      = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    container_name       = "state"
+    key                  = "vault.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
