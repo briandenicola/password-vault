390ca9e728f36779fc6e2577f7e930ab9b9e97a9
Brian <brian@briandenicola.com>
2025-01-10 17:21:05 +0000

    Bug fixes for PWA

--- src/passwordapp.ui/vue.config.js
+++ src/passwordapp.ui/vue.config.js
@@ -1,35 +1,72 @@
 module.exports = {
     pwa: {
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-      short_name: "Vault",
-      name: "Password Vault",
-      start_url: ".",
-      display: "standalone",
-      orientation: "portrait",
-      background_color: "#343541",
-      theme_color: "#202A44",
-      icons: [
-        {
-          "src": "/img/icons/favicon-16x16.png",
-          "sizes": "16x16",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/favicon-32x32.png",
-          "sizes": "32x32",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/favicon-64x64.png",
-          "sizes": "64x64",
-          "type": "image/png"
-        },      
-        {
-          "src": "/img/icons/apple-touch-icon.png",
-          "sizes": "32x32",
-          "type": "image/png"
-        }      
-      ]      
-    },
-  }
+        name: "Password Vault",
+        start_url: ".",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#343541",
+        themeColor: "#202A44",
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        icons: [
+            {
+                src: "/img/icons/favicon-16x16.png",
+                sizes: "16x16",
+                type: "image/png"
+            },
+            {
+                src: "/img/icons/favicon-32x32.png",
+                sizes: "32x32",
+                type: "image/png"
+            },
+            {
+                src: "/img/icons/favicon-64x64.png",
+                sizes: "64x64",
+                type: "image/png"
+            },
+            {
+                src: "/img/icons/apple-touch-icon.png",
+                sizes: "32x32",
+                type: "image/png"
+            }
+        ]
+    }
+}
+module.exports = {
+    pwa: {
+        name: 'Password Vault',
+        themeColor: '#202A44',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black-translucent',
+        manifestOptions: {
+            start_url: '.',
+            display: 'standalone',
+            orientation: "portrait",
+            background_color: "#343541",
+            short_name: "Vault",
+            icons: [
+                {
+                    src: "/img/icons/favicon-16x16.png",
+                    sizes: "16x16",
+                    type: "image/png"
+                },
+                {
+                    src: "/img/icons/favicon-32x32.png",
+                    sizes: "32x32",
+                    type: "image/png"
+                },
+                {
+                    src: "/img/icons/favicon-64x64.png",
+                    sizes: "64x64",
+                    type: "image/png"
+                },
+                {
+                    src: "/img/icons/apple-touch-icon.png",
+                    sizes: "32x32",
+                    type: "image/png"
+                }
+            ]
+        }
+    }
+};
