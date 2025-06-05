3658e33868f5e950e88a4c145c1e6a322e678e77
briandenicola <brian@briandenicola.com>
2025-06-05 09:48:30 -0500

    ⚽ Thu Jun  5 09:48:30 CDT 2025

--- docs/deploy.md
+++ docs/deploy.md
@@ -7,7 +7,7 @@ API Build & Deployment
 
 # Steps
 - :one: `task deploy-api`          - Builds and deploys the API to Azure Functions
-- :two: Manually add the Azure Static Web Apps Default domain and your Custom Domain to the Azure Functions CORS settings.  This is required for the UI to call the API.
+- :two: `task cors`                - Configures CORS for the API to allow access from the UI
 <p align="right">(<a href="#build">back to top</a>)</p>
 
 
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,50 +1,55 @@
 output "APP_NAME" {
-    value = local.resource_name
-    sensitive = false
+  value     = local.resource_name
+  sensitive = false
 }
 
 output "APP_RESOURCE_GROUP" {
-    value = azurerm_resource_group.this.name
-    sensitive = false
+  value     = azurerm_resource_group.this.name
+  sensitive = false
 }
 
 output "SWA_NAME" {
-    value = azurerm_static_web_app.this.name
-    sensitive = false
+  value     = azurerm_static_web_app.this.name
+  sensitive = false
 }
 
 output "STORAGE_ACCOUNT_NAME" {
-    value = azurerm_storage_account.this.name
-    sensitive = false
+  value     = azurerm_storage_account.this.name
+  sensitive = false
 }
 
 output "FUNCTION_NAME" {
-    value = azurerm_linux_function_app.this.name
-    sensitive = false
+  value     = azurerm_linux_function_app.this.name
+  sensitive = false
 }
 
 output "FUNCTION_URL" {
-    value = "https://${azurerm_linux_function_app.this.default_hostname}"
-    sensitive = false
+  value     = "https://${azurerm_linux_function_app.this.default_hostname}"
+  sensitive = false
 }
 
 output "AZURE_FUNCTION_HOST_KEY" {
-    value = data.azurerm_function_app_host_keys.host_key.primary_key
-    sensitive = true
+  value     = data.azurerm_function_app_host_keys.host_key.primary_key
+  sensitive = true
 }
 
 
 output "APP_INSIGHTS_CONNECTION_STRING" {
-    value = azurerm_application_insights.this.connection_string
-    sensitive = true
+  value     = azurerm_application_insights.this.connection_string
+  sensitive = true
 }
 
 output "MAINTENANCE_FUNCTION_NAME" {
-    value = length(module.maintenance) > 0 ? module.maintenance[0].FUNCTION_NAME :  null
-    sensitive = false
+  value     = length(module.maintenance) > 0 ? module.maintenance[0].FUNCTION_NAME : null
+  sensitive = false
 }
 
 output "MAINTENANCE_STORAGE_ACCOUNT_NAME" {
-    value = length(module.maintenance) > 0 ? module.maintenance[0].STORAGE_ACCOUNT_NAME :  null
-    sensitive = false
+  value     = length(module.maintenance) > 0 ? module.maintenance[0].STORAGE_ACCOUNT_NAME : null
+  sensitive = false
+}
+
+output "SWA_DEFAULT_URL" {
+  value     = "https://${azurerm_static_web_app.this.default_host_name}"
+  sensitive = false
 }
